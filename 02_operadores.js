783a88560d4baa5aa29cecd1449ad4c17187e056
Saul Dominguez <saul.vau@gmail.com>
2023-12-04 22:33:56 +0100

    primer comit de java

---
+++ 02_operadores.js
@@ -0,0 +1,44 @@
+//asignacion
+let x = 10;
+let y =5;
+
+//aritmeticos
+
+console.log(x+y);
+
+const suma = x+y;
+
+console.log(suma);
+
+const diferencia = suma/2;
+
+console.log(diferencia);
+
+const modulo = 4%2;
+
+console.log(`modulo ${modulo}`);
+
+console.log('Hola '+ 'mundo');
+
+console.log('12'+1);
+
+console.log('12'-1);
+
+x+=50; //x = x+50;
+x++ //x = x+1;
+
+console.log(x);
+
+/// operadores de comparacion
+
+console.log('Hola' == 'Hola');
+console.log('12' == 12);
+
+console.log('12' === 12);
+console.log('Hola' !== 'Hola');
+
+console.log(x<y);
+console.log(x>y);
+console.log(x<=y);
+
+console.log(NaN === NaN);
